411b9bd33bfd1d9ecea750282645d3e6fe8b9810
unknown <foo@example.com>
2013-08-29 14:05:41 +0900

    attr(ryまで

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  attr_accessible :title, :content
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,4 @@
 class Work < ActiveRecord::Base
   belongs_to :user
+  attr_accessible :title, :content, :url
 end
