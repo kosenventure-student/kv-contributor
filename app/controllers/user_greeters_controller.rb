02bae169d6e7450b1426ea4847140be2df3275e9
daiki <corsubacies@gmail.com>
2013-08-30 12:32:29 +0900

    更新

--- app/controllers/user_greeters_controller.rb
+++ app/controllers/user_greeters_controller.rb
@@ -1,9 +1,9 @@
-class UserGreetersController < ApplicationController
+﻿class UserGreetersController < ApplicationController
   def greet
     userinfo_ = params[:uid]
     @userinfo = User.find_by(:id => userinfo_.to_i)
     if @userinfo == nil
-      render :text => "不明なユーザ : #{params[:uid]}"
+      render :text => "#{params[:uid]}"
     end
   end
 end
